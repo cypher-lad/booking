63f1ded0829078d9a2712423ed1b91fef5a29889
Mark Ramirez <mrk.anthony.rmrez@gmail.com>
2022-10-04 13:24:55 +0800

    Convert static html to go tmpl

--- pkg/handlers/handlers.go
+++ pkg/handlers/handlers.go
@@ -49,3 +49,43 @@ func (m *Repository) About(w http.ResponseWriter, r *http.Request) {
 		StringMap: stringMap,
 	})
 }
+
+// Generals is the Generals Quarters page handler
+func (m *Repository) Generals(w http.ResponseWriter, r *http.Request) {
+	remoteIP := r.RemoteAddr
+	m.App.Session.Put(r.Context(), "remote_ip", remoteIP)
+
+	render.RenderTemplate(w, "generals.page.tmpl", &models.TemplateData{})
+}
+
+// Majors is the Majors Suite page handler
+func (m *Repository) Majors(w http.ResponseWriter, r *http.Request) {
+	remoteIP := r.RemoteAddr
+	m.App.Session.Put(r.Context(), "remote_ip", remoteIP)
+
+	render.RenderTemplate(w, "majors.page.tmpl", &models.TemplateData{})
+}
+
+// MakeReservation is the Make Reservation page handler
+func (m *Repository) MakeReservation(w http.ResponseWriter, r *http.Request) {
+	remoteIP := r.RemoteAddr
+	m.App.Session.Put(r.Context(), "remote_ip", remoteIP)
+
+	render.RenderTemplate(w, "make-reservation.page.tmpl", &models.TemplateData{})
+}
+
+// SearchAvailability is the Search Availability page handler
+func (m *Repository) SearchAvailability(w http.ResponseWriter, r *http.Request) {
+	remoteIP := r.RemoteAddr
+	m.App.Session.Put(r.Context(), "remote_ip", remoteIP)
+
+	render.RenderTemplate(w, "search-availability.page.tmpl", &models.TemplateData{})
+}
+
+// Contact is the Contact page handler
+func (m *Repository) Contact(w http.ResponseWriter, r *http.Request) {
+	remoteIP := r.RemoteAddr
+	m.App.Session.Put(r.Context(), "remote_ip", remoteIP)
+
+	render.RenderTemplate(w, "contact.page.tmpl", &models.TemplateData{})
+}
